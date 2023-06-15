9564e7d5550c085d2c69cb00a82264fdc5321e0f
Dmitry <dev@dergunov.info>
2023-06-15 14:05:17 +0300

    отключен CORS

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        https: true,
-        host: 'localhost',
-    },
+    // server: {
+    //     https: true,
+    //     host: 'https://xn--b1abce5bglz.xn--p1ai',
+    // },
     plugins: [
         laravel({
             input: [
