ddca2ef27abab9ecbf20feaa336ad679b28316a7
Dmitry <dev@dergunov.info>
2023-06-23 15:27:38 +0300

    Вкладки в блоке TokyoHomeBlock

--- app/Filament/Fabricator/PageBlocks/Tokyo/TokyoHomeBlock.php
+++ app/Filament/Fabricator/PageBlocks/Tokyo/TokyoHomeBlock.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Fabricator\PageBlocks\Tokyo;
 
+use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Toggle;
@@ -18,49 +19,58 @@ class TokyoHomeBlock extends PageBlock
     return Block::make('tokyo.tokyo-home')
       ->visible(fn ($get) => $get('../layout') == 'tokyo')
       ->schema([
-        Group::make()
-          ->schema([
-            TextInput::make('title')->label(__('Заголовок:')),
-            TextInput::make('subtitle')->label(__('Подзаголовок:')),
-            CuratorPicker::make('photo')
-              ->label('Фотография:')
-              ->buttonLabel('UploadImage')
-              ->color('primary')
-              ->outlined(false)
-              ->size('md'),
-          ]),
-
-        Group::make()
-          ->schema([
-            Toggle::make('background')
-              ->label('Фон')
-              ->inline(),
-            Radio::make('type')
-              ->label('Рамка картинки')
-              ->options([
-                'wave' => 'Волна',
-                'circle' => 'Круглая',
-                'square' => 'Квадратная'
-              ])
-              ->default('wave')
-              ->inline()
-              ->columnSpan(2),
-          ])->columns(3),
-
-        Group::make()
-          ->schema([
-            Repeater::make('socials')
+        Tabs::make('home')
+          ->tabs([
+            Tabs\Tab::make('main')
+              ->label(__('Основное'))
               ->schema([
-                TextInput::make('url')
-                  ->label(__('Ссылка:'))
-                  ->default('#')
-                  ->columnSpan(2),
-                TextInput::make('icon')
-                  ->label(__('Иконка:'))
-                  ->default('icon-vkontakte-1'),
+                Group::make()
+                  ->schema([
+                    TextInput::make('title')->label(__('Заголовок:')),
+                    TextInput::make('subtitle')->label(__('Подзаголовок:')),
+                    CuratorPicker::make('photo')
+                      ->label('Фотография:')
+                      ->buttonLabel('UploadImage')
+                      ->color('primary')
+                      ->outlined(false)
+                      ->size('md'),
+                  ]),
+
+                Group::make()
+                  ->schema([
+                    Toggle::make('background')
+                      ->label('Фон')
+                      ->inline(),
+                    Radio::make('type')
+                      ->label('Рамка картинки')
+                      ->options([
+                        'wave' => 'Волна',
+                        'circle' => 'Круглая',
+                        'square' => 'Квадратная'
+                      ])
+                      ->default('wave')
+                      ->inline()
+                      ->columnSpan(2),
+                  ])->columns(3),
+              ]),
+
+            Tabs\Tab::make('socials')
+              ->label(__('Соцсети'))
+              ->schema([
+                Repeater::make('socials')
+                  ->schema([
+                    TextInput::make('url')
+                      ->label(__('Ссылка:'))
+                      ->default('#')
+                      ->columnSpan(2),
+                    TextInput::make('icon')
+                      ->label(__('Иконка:'))
+                      ->default('icon-vkontakte-2'),
+                  ])
+                  ->columns(3)
               ])
-              ->columns(3)
           ])
+
       ]);
   }
 
@@ -69,3 +79,31 @@ class TokyoHomeBlock extends PageBlock
     return $data;
   }
 }
+
+
+// Tabs::make('home')
+// ->tabs([
+//   Tabs\Tab::make('main')
+//   ->label(__('Основное'))
+//   ->schema([
+//     Group::make()
+//       ->schema([
+// //
+//       ]),
+
+//     Group::make()
+//       ->schema([
+// //
+//       ])->columns(3),
+//   ]),
+
+//   Tabs\Tab::make('socials')
+//   ->label(__('Соцсети'))
+//   ->schema([
+//     Repeater::make('socials')
+//     ->schema([
+// //
+//     ])
+//       ->columns(3)
+//   ])
+// ])
