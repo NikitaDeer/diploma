b3b021b6258bf462cb07494e3ad73fe2877d83c2
Dmitry <dev@dergunov.info>
2023-06-24 10:13:36 +0300

    блок TokyoAboutBlock переписан ChatGPT для удобочитаемости

--- app/Filament/Fabricator/PageBlocks/Tokyo/TokyoAboutBlock.php
+++ app/Filament/Fabricator/PageBlocks/Tokyo/TokyoAboutBlock.php
@@ -19,121 +19,146 @@ class TokyoAboutBlock extends PageBlock
       ->visible(fn ($get) => $get('../layout') == 'tokyo')
       ->schema([
         Tabs::make('about')
-          ->tabs([
-            Tabs\Tab::make('About me')
-              ->label(__('Основное'))
-              ->schema([
-                TextInput::make('title')
-                  ->label(__('Заголовок:')),
-                CuratorPicker::make('photo')
-                  ->label('Фотография:')
-                  ->buttonLabel('UploadImage')
-                  ->color('primary')
-                  ->outlined(false)
-                  ->size('md'),
-
-                Group::make()
-                  ->schema([
-                    TextInput::make('subtitle')
-                      ->label(__('Подзаголовок:')),
-                    TextInput::make('curation')
-                      ->label(__('curator-curation:')),
-                    TextInput::make('description')
-                      ->label(__('Описание:'))
-                      ->columnSpan(2),
-                  ])->columns(2),
-
-                Textarea::make('text')
-                  ->label(__('Текст:')),
-              ]),
-
-            Tabs\Tab::make('short-info')
-              ->label(__('Коротко'))
-              ->schema([
-                Repeater::make('short-info-left')
-                  ->schema([
-                    TextInput::make('short-title')
-                      ->label(__('Параметр:')),
-                    TextInput::make('short-text')
-                      ->label(__('Данные:')),
-                    TextInput::make('short-url')
-                      ->label(__('Ссылка:'))
-                      ->columnSpan(2),
-                  ])
-                  ->columns(2),
-
-                Repeater::make('short-info-right')
-                  ->schema([
-                    TextInput::make('short-title')
-                      ->label(__('Параметр:')),
-                    TextInput::make('short-text')
-                      ->label(__('Данные:')),
-                  ])
-                  ->columns(2)
-              ]),
-
-            Tabs\Tab::make('Education')
-              ->label(__('Образование'))
-              ->schema([
-                Group::make()
-                  ->schema([
-                    TextInput::make('education-left-title')
-                      ->label(__('Название левой группы:')),
-                    Repeater::make('education-left')
-                      ->label(__('Левая группа:'))
-                      ->schema([
-                        TextInput::make('year')
-                          ->label(__('Годы:')),
-                        TextInput::make('place')
-                          ->label(__('Место:')),
-                        TextInput::make('description')
-                          ->label(__('Описание:')),
-                      ]),
-                  ]),
-
-                Group::make()
-                  ->schema([
-                    TextInput::make('education-right-title')
-                      ->label(__('Название правой группы:')),
-                    Repeater::make('education-right')
-                      ->label(__('Правая группа:'))
-                      ->schema([
-                        TextInput::make('year')
-                          ->label(__('Годы:')),
-                        TextInput::make('place')
-                          ->label(__('Место:')),
-                        TextInput::make('description')
-                          ->label(__('Описание:')),
-                      ]),
-                  ]),
-              ]),
-
-            Tabs\Tab::make('Testimonials')
-              ->label(__('Отзывы'))
-              ->schema([
-                TextInput::make('testimonials-title')
-                  ->label(__('Название:'))
-                  ->placeholder('Только положительные отзывы:'),
-                Repeater::make('testimonials')
-                  ->schema([
-                    CuratorPicker::make('photo')
-                      ->label(__('Фотография:'))
-                      ->buttonLabel(__('Загрузить'))
-                      ->color('primary')
-                      ->outlined(false)
-                      ->size('sm'),
-                    TextInput::make('curation')
-                      ->label(__('curator-curation:')),
-                    TextInput::make('title')
-                      ->label(__('Имя:')),
-                    TextInput::make('description')
-                      ->label(__('Описание:')),
-                    TextInput::make('text')
-                      ->label(__('Текст:')),
-                  ]),
-              ]),
-          ])
+          ->tabs(static::getTabsSchema()),
       ]);
+  }
+
+  protected static function getTabsSchema(): array
+  {
+    return [
+      Tabs\Tab::make('About me')
+        ->label(__('Основное'))
+        ->schema(static::getAboutMeSchema()),
+      Tabs\Tab::make('short-info')
+        ->label(__('Коротко'))
+        ->schema(static::getShortInfoSchema()),
+      Tabs\Tab::make('Education')
+        ->label(__('Образование'))
+        ->schema(static::getEducationSchema()),
+      Tabs\Tab::make('Testimonials')
+        ->label(__('Отзывы'))
+        ->schema(static::getTestimonialsSchema()),
+    ];
+  }
+
+  protected static function getAboutMeSchema(): array
+  {
+    return [
+      TextInput::make('title')
+        ->label(__('Заголовок:')),
+      CuratorPicker::make('photo')
+        ->label('Фотография:')
+        ->buttonLabel('UploadImage')
+        ->color('primary')
+        ->outlined(false)
+        ->size('md'),
+
+      Group::make()
+        ->schema([
+          TextInput::make('subtitle')
+            ->label(__('Подзаголовок:')),
+          TextInput::make('curation')
+            ->label(__('curator-curation:')),
+          TextInput::make('description')
+            ->label(__('Описание:'))
+            ->columnSpan(2),
+        ])->columns(2),
+
+      Textarea::make('text')
+        ->label(__('Текст:')),
+    ];
+  }
+
+  protected static function getShortInfoSchema(): array
+  {
+    return [
+      Repeater::make('short-info-left')
+        ->schema([
+          TextInput::make('short-title')
+            ->label(__('Параметр:')),
+          TextInput::make('short-text')
+            ->label(__('Данные:')),
+          TextInput::make('short-url')
+            ->label(__('Ссылка:'))
+            ->columnSpan(2),
+        ])
+        ->columns(2),
+
+      Repeater::make('short-info-right')
+        ->schema([
+          TextInput::make('short-title')
+            ->label(__('Параметр:')),
+          TextInput::make('short-text')
+            ->label(__('Данные:')),
+        ])
+        ->columns(2)
+    ];
+  }
+
+  protected static function getEducationSchema(): array
+  {
+    return [
+      Group::make()
+        ->schema([
+          TextInput::make('education-left-title')
+            ->label(__('Название левой группы:')),
+
+          Repeater::make('education-left')
+            ->label(__('Левая группа:'))
+            ->schema([
+              TextInput::make('year')
+                ->label(__('Годы:')),
+              TextInput::make('place')
+                ->label(__('Место:')),
+              TextInput::make('description')
+                ->label(__('Описание:')),
+            ]),
+        ]),
+
+      Group::make()
+        ->schema([
+          TextInput::make('education-right-title')
+            ->label(__('Название правой группы:')),
+
+          Repeater::make('education-right')
+            ->label(__('Правая группа:'))
+            ->schema([
+              TextInput::make('year')
+                ->label(__('Годы:')),
+              TextInput::make('place')
+                ->label(__('Место:')),
+              TextInput::make('description')
+                ->label(__('Описание:')),
+            ]),
+        ]),
+    ];
+  }
+
+  protected static function getTestimonialsSchema(): array
+  {
+    return [
+      TextInput::make('testimonials-title')
+        ->label(__('Название:'))
+        ->placeholder('Только положительные отзывы:'),
+
+      Repeater::make('testimonials')
+        ->schema([
+          CuratorPicker::make('photo')
+            ->label(__('Фотография:'))
+            ->buttonLabel(__('Загрузить'))
+            ->color('primary')
+            ->outlined(false)
+            ->size('sm'),
+          TextInput::make('curation')
+            ->label(__('curator-curation:')),
+          TextInput::make('title')
+            ->label(__('Имя:')),
+          TextInput::make('description')
+            ->label(__('Описание:')),
+          TextInput::make('text')
+            ->label(__('Текст:')),
+        ]),
+    ];
   }
 
   public static function mutateData(array $data): array
@@ -141,8 +166,3 @@ class TokyoAboutBlock extends PageBlock
     return $data;
   }
 }
-
-        // Group::make()
-        //   ->schema([
-
-        //   ]),
