99c36989957a5480652b1270834702867b31cd61
Dmitry-Wsl <dev@dergunov.info>
2023-06-30 19:09:45 +0300

    services

--- app/Filament/Fabricator/PageBlocks/Tokyo/TokyoServiceBlock.php
+++ app/Filament/Fabricator/PageBlocks/Tokyo/TokyoServiceBlock.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Fabricator\PageBlocks\Tokyo;
 
-use Filament\Forms\Components\Tabs;
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class TokyoServiceBlock extends PageBlock
@@ -11,8 +15,10 @@ class TokyoServiceBlock extends PageBlock
   public static function getBlockSchema(): Block
   {
     return Block::make('tokyo.tokyo-service')
-      ->visible(fn ($get) => $get('../layout') == 'tokyo')
+      ->visible(fn($get) => $get('../layout') == 'tokyo')
       ->schema([
+        TextInput::make('page-title')
+          ->label(__('Заголовок страницы:')),
         Tabs::make('service')
           ->tabs(static::getTabsSchema()),
       ]);
@@ -24,36 +30,110 @@ class TokyoServiceBlock extends PageBlock
       Tabs\Tab::make('Services')
         ->label(__('Что мы можем'))
         ->schema(static::getServicesSchema()),
-      Tabs\Tab::make('Partners')
-        ->label(__('Наши партнеры'))
-        ->schema(static::getPartnersSchema()),
-      Tabs\Tab::make('Facts')
-        ->label(__('Интересные факты'))
-        ->schema(static::getFactsSchema()),
+      // Пока секция не нужна
+      // Tabs\Tab::make('Partners')
+      //   ->label(__('Наши партнеры'))
+      //   ->schema(static::getPartnersSchema()),
+
       Tabs\Tab::make('Pricing')
         ->label(__('Цены'))
         ->schema(static::getPricingSchema()),
+      Tabs\Tab::make('Facts')
+        ->label(__('Интересные факты'))
+        ->schema(static::getFactsSchema()),
     ];
   }
 
   protected static function getServicesSchema(): array
   {
-    return [];
+    return [
+      TextInput::make('services-title')
+        ->label(__('Название секции:')),
+      Repeater::make('services')
+        ->schema([
+          TextInput::make('title')
+            ->label(__('Заголовок:')),
+          Tabs::make('')
+            ->tabs([
+              Tabs\Tab::make('basic')
+                ->label(__('Основное'))
+                ->schema(static::getBasicSchema()),
+              Tabs\Tab::make('detail')
+                ->label(__('Подробности'))
+                ->schema(static::getDetailSchema()),
+            ]),
+        ]),
+    ];
+  }
+  protected static function getBasicSchema(): array
+  {
+    return [
+      // TODO добавить ограничение на количество строк
+      Textarea::make('text')
+        ->label(__(' Короткий текст:')),
+    ];
   }
 
-  protected static function getPartnersSchema(): array
+  protected static function getDetailSchema(): array
   {
-    return [];
-  }
-
-  protected static function getFactsSchema(): array
-  {
-    return [];
+    return [
+      // CuratorPicker::make('image')
+      //   ->label('Иллюстрация:')
+      //   ->buttonLabel(__('Загрузить:'))
+      //   ->color('primary')
+      //   ->outlined(false)
+      //   ->size('sm'),
+      MarkdownEditor::make('detail-text')
+        ->label('Подробнее:')
+        ->disableAllToolbarButtons()
+        ->enableToolbarButtons([
+          'bold',
+          'bulletList',
+          'codeBlock',
+          'edit',
+          'italic',
+          'link',
+          'orderedList',
+          'preview',
+          'strike',
+        ]),
+    ];
   }
 
   protected static function getPricingSchema(): array
   {
-    return [];
+    return [
+      TextInput::make('pricing-title')
+        ->label(__('Название секции:')),
+      Repeater::make('pricing')
+        ->schema([
+
+        ])
+    ];
+  }
+
+  protected static function getPartnersSchema(): array
+  {
+    return [
+      TextInput::make('partners-title')
+        ->label(__('Название секции:')),
+      Repeater::make('partners')
+        ->schema([
+
+        ])
+    ];
+  }
+
+  protected static function getFactsSchema(): array
+  {
+    return [
+      TextInput::make('facts-title')
+        ->label(__('Название секции:')),
+      Repeater::make('facts')
+        ->schema([
+
+        ])
+    ];
   }
 
   public static function mutateData(array $data): array
--- resources/views/components/filament-fabricator/page-blocks/tokyo/tokyo-service.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/tokyo/tokyo-service.blade.php
@@ -1,4 +1,4 @@
-@aware(['page'])
+@aware(['page', 'pageTitle', 'services'])
 <!-- SERVICE -->
 <div id="service" class="tokyo_tm_section">
 
@@ -8,134 +8,64 @@
         <div class="title_flex">
           <div class="left">
             <span>Services</span>
-            <h3>What I Do</h3>
+            <h3>{{ $pageTitle }}</h3>
           </div>
         </div>
       </div>
       <div class="list">
         <ul>
+          @foreach ($services as $service)
+            <li>
+              <div class="list_inner">
+                <span class="number">{{ $loop->iteration }}</span>
+                <h3 class="title">{{ $service['title'] }}</h3>
+                <p class="text">{{ $service['text'] }}</p>
+                <div class="tokyo_tm_read_more">
+                  <a href="#"><span>Подробнее</span></a>
+                </div>
+                <a class="tokyo_tm_full_link" href="#"></a>
 
-          <li>
-            <div class="list_inner">
-              <span class="number">01</span>
-              <h3 class="title">Web Design</h3>
-              <p class="text">Web development is the most famous job in the world and it is very
-                interesting...</p>
-              <div class="tokyo_tm_read_more">
-                <a href="#"><span>Read More</span></a>
-              </div>
-              <a class="tokyo_tm_full_link" href="#"></a>
-
-              <!-- Service Popup Start -->
-              <img class="popup_service_image" src="{{ asset('storage/tokyo/img/news/1.jpg') }}" alt="" />
-              <div class="service_hidden_details">
-                <div class="service_popup_informations">
-                  <div class="descriptions">
-                    <p>Tokyo is a leading web design agency with an award-winning design team that creates
-                      innovative, effective websites that capture your brand, improve your conversion rates,
-                      and maximize your revenue to help grow your business and achieve your goals.</p>
-                    <p>In today’s digital world, your website is the first interaction consumers have with
-                      your business. That's why almost 95 percent of a user’s first impression relates to web
-                      design. It’s also why web design services can have an immense impact on your company’s
-                      bottom line.</p>
-                    <p>That’s why more companies are not only reevaluating their website’s design but also
-                      partnering with Tokyo, the web design agency that’s driven more than $2.4 billion in
-                      revenue for its clients. With over 50 web design awards under our belt, we're confident
-                      we can design a custom website that drives sales for your unique business.</p>
+                <!-- Service Popup Start -->
+                <img class="popup_service_image" src="{{ asset('storage/tokyo/img/news/1.jpg') }}" alt="" />
+                {{-- <x-curator-glider class="popup_service_image" :media="$service['image']" /> --}}
+                <div class="service_hidden_details">
+                  <div class="service_popup_informations">
+                    <div class="descriptions">
+                      <x-markdown>
+                        {{ $service['detail-text'] }}
+                      </x-markdown>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <!-- /Service Popup End -->
+                <!-- /Service Popup End -->
 
-            </div>
-          </li>
+              </div>
+            </li>
+          @endforeach
 
         </ul>
       </div>
     </div>
   </div>
 
-  <div class="tokyo_tm_partners">
+  {{-- <div class="tokyo_tm_partners">
     <div class="container">
       <div class="tokyo_section_title">
         <h3>Partners</h3>
       </div>
       <div class="partners_inner">
         <ul>
+
           <li>
             <div class="list_inner">
               <img src="{{ asset('storage/tokyo/img/partners/dark/1.png') }}" alt="" />
             </div>
           </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/2.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/3.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/4.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/5.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/6.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/7.png') }}" alt="" />
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <img src="{{ asset('storage/tokyo/img/partners/dark/8.png') }}" alt="" />
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
 
-  <div class="tokyo_tm_facts">
-    <div class="container">
-      <div class="tokyo_section_title">
-        <h3>Fun Facts</h3>
-      </div>
-      <div class="list">
-        <ul>
-          <li>
-            <div class="list_inner">
-              <h3>777+</h3>
-              <span>Projects Completed</span>
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <h3>3K</h3>
-              <span>Happy Clients</span>
-            </div>
-          </li>
-          <li>
-            <div class="list_inner">
-              <h3>9K+</h3>
-              <span>Lines of Code</span>
-            </div>
-          </li>
         </ul>
       </div>
     </div>
-  </div>
+  </div> --}}
 
   <div class="tokyo_tm_pricing">
     <div class="container">
@@ -144,35 +74,6 @@
       </div>
       <div class="list">
         <ul>
-          <li>
-            <div class="list_inner">
-              <div class="price">
-                <h3><span>0<span class="currency">$</span></span></h3>
-              </div>
-              <div class="plan">
-                <h3>Free</h3>
-              </div>
-              <ul class="item">
-                <li class="active">
-                  <p>Premium Icons</p>
-                </li>
-                <li>
-                  <p>Quality Logo</p>
-                </li>
-                <li>
-                  <p>Stock Images</p>
-                </li>
-                <li>
-                  <p>Free Support</p>
-                </li>
-              </ul>
-              <div class="tokyo_tm_button" data-position="left">
-                <a href="#">
-                  <span>Purchase</span>
-                </a>
-              </div>
-            </div>
-          </li>
           <li>
             <div class="list_inner">
               <div class="price">
@@ -203,35 +104,27 @@
               <span class="popular">Popular</span>
             </div>
           </li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="tokyo_tm_facts">
+    <div class="container">
+      <div class="tokyo_section_title">
+        <h3>Fun Facts</h3>
+      </div>
+      <div class="list">
+        <ul>
+
           <li>
             <div class="list_inner">
-              <div class="price">
-                <h3><span>70<span class="currency">$</span></span></h3>
-              </div>
-              <div class="plan">
-                <h3>Premium</h3>
-              </div>
-              <ul class="item">
-                <li class="active">
-                  <p>Premium Icons</p>
-                </li>
-                <li class="active">
-                  <p>Quality Logo</p>
-                </li>
-                <li class="active">
-                  <p>Stock Images</p>
-                </li>
-                <li class="active">
-                  <p>Free Support</p>
-                </li>
-              </ul>
-              <div class="tokyo_tm_button" data-position="left">
-                <a href="#">
-                  <span>Purchase</span>
-                </a>
-              </div>
+              <h3>777+</h3>
+              <span>Projects Completed</span>
             </div>
           </li>
+
         </ul>
       </div>
     </div>
